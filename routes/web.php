e4b3c018926844bbd53b258b3350956a44ad794e
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-11-29 13:43:54 -0800

    initial routes for pages

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Homepage
 Route::get('/', function () {
+    return view('welcome');
+});
+
+// Show All Listings
+Route::get('/{property_type}/{listing_type}', function () {
+    return view('welcome');
+});
+
+// Single Listing
+Route::get('/listing/{slug}/{id}', function () {
+    return view('welcome');
+});
+
+// User Login
+Route::get('/login', function () {
+    return view('welcome');
+});
+
+// User Register
+Route::get('/register', function () {
+    return view('welcome');
+});
+
+// User Saved Listings
+Route::get('/account/saved', function () {
+    return view('welcome');
+});
+
+// User Show Status
+Route::get('/account/show-status', function () {
     return view('welcome');
 });
