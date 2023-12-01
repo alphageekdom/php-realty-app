f725cbab1547bb76dead49068f72f2e4f550cb25
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-12-01 15:55:13 -0800

    listings cards and responsiveness

--- resources/views/pages/listings.blade.php
+++ resources/views/pages/listings.blade.php
@@ -27,6 +27,102 @@
             </div>
         </div>
     </div>
+    <div class="listings-properties">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-lg-4 col-xl-3">
+                    <div class="listings-properties__item">
+                        <img src="https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=3275&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Property Image" />
+                        <span class="listings-properties__item-price">$250,000</span>
+                        <span class="listings-properties__item-details">BD <i class="fa-solid fa-bed"></i> 4 BA <i class="fa-solid fa-bath"></i> 3 SQ <i class="fa-solid fa-ruler"></i> 2,340</span>
+                        <span class="listings-properties__item-address">
+                            417 Champlin Ridges, <br> 
+                            Thaddeusview, RI 10040
+                        </span>
+                        <div class="listings-properties__item-line"></div>
+                        <span class="listings-properties__item-owner">
+                            Tommie R. Williams Realty
+                        </span>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 col-xl-3">
+                    <div class="listings-properties__item">
+                        <img src="https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=3275&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Property Image" />
+                        <span class="listings-properties__item-price">$250,000</span>
+                        <span class="listings-properties__item-details">BD <i class="fa-solid fa-bed"></i> 4 BA <i class="fa-solid fa-bath"></i> 3 SQ <i class="fa-solid fa-ruler"></i> 2,340</span>
+                        <span class="listings-properties__item-address">
+                            417 Champlin Ridges, <br> 
+                            Thaddeusview, RI 10040
+                        </span>
+                        <div class="listings-properties__item-line"></div>
+                        <span class="listings-properties__item-owner">
+                            Tommie R. Williams Realty
+                        </span>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 col-xl-3">
+                    <div class="listings-properties__item">
+                        <img src="https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=3275&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Property Image" />
+                        <span class="listings-properties__item-price">$250,000</span>
+                        <span class="listings-properties__item-details">BD <i class="fa-solid fa-bed"></i> 4 BA <i class="fa-solid fa-bath"></i> 3 SQ <i class="fa-solid fa-ruler"></i> 2,340</span>
+                        <span class="listings-properties__item-address">
+                            417 Champlin Ridges, <br> 
+                            Thaddeusview, RI 10040
+                        </span>
+                        <div class="listings-properties__item-line"></div>
+                        <span class="listings-properties__item-owner">
+                            Tommie R. Williams Realty
+                        </span>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 col-xl-3">
+                    <div class="listings-properties__item">
+                        <img src="https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=3275&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Property Image" />
+                        <span class="listings-properties__item-price">$250,000</span>
+                        <span class="listings-properties__item-details">BD <i class="fa-solid fa-bed"></i> 4 BA <i class="fa-solid fa-bath"></i> 3 SQ <i class="fa-solid fa-ruler"></i> 2,340</span>
+                        <span class="listings-properties__item-address">
+                            417 Champlin Ridges, <br> 
+                            Thaddeusview, RI 10040
+                        </span>
+                        <div class="listings-properties__item-line"></div>
+                        <span class="listings-properties__item-owner">
+                            Tommie R. Williams Realty
+                        </span>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 col-xl-3">
+                    <div class="listings-properties__item">
+                        <img src="https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=3275&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Property Image" />
+                        <span class="listings-properties__item-price">$250,000</span>
+                        <span class="listings-properties__item-details">BD <i class="fa-solid fa-bed"></i> 4 BA <i class="fa-solid fa-bath"></i> 3 SQ <i class="fa-solid fa-ruler"></i> 2,340</span>
+                        <span class="listings-properties__item-address">
+                            417 Champlin Ridges, <br> 
+                            Thaddeusview, RI 10040
+                        </span>
+                        <div class="listings-properties__item-line"></div>
+                        <span class="listings-properties__item-owner">
+                            Tommie R. Williams Realty
+                        </span>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 col-xl-3">
+                    <div class="listings-properties__item">
+                        <img src="https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=3275&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Property Image" />
+                        <span class="listings-properties__item-price">$250,000</span>
+                        <span class="listings-properties__item-details">BD <i class="fa-solid fa-bed"></i> 4 BA <i class="fa-solid fa-bath"></i> 3 SQ <i class="fa-solid fa-ruler"></i> 2,340</span>
+                        <span class="listings-properties__item-address">
+                            417 Champlin Ridges, <br> 
+                            Thaddeusview, RI 10040
+                        </span>
+                        <div class="listings-properties__item-line"></div>
+                        <span class="listings-properties__item-owner">
+                            Tommie R. Williams Realty
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 Testing
 @endsection
