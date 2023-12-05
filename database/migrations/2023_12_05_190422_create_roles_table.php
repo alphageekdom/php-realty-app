e6d0e68eef0019fa37d9e66d2f134e303f9c9537
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-12-05 12:19:02 -0800

    set roles for users, migrations to create roles and seeders for roles

--- database/migrations/2023_12_05_190422_create_roles_table.php
+++ database/migrations/2023_12_05_190422_create_roles_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('listings', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
             $table->timestamps();
         });
     }
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('listings');
+        Schema::dropIfExists('roles');
     }
 };
